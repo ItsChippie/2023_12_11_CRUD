e0a07cb3e44210691f232e8d95427b07045b6f40
Miklós Rajmund <pacmagic75@gmail.com>
2023-12-11 10:14:13 +0100

    elso

---
+++ BooksApp/Models/books.cs
@@ -0,0 +1,10 @@
+﻿namespace BooksApp.Models
+{
+    public class books
+    {   
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+    }
+}
